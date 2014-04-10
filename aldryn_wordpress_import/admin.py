795dd435b66acfc55a8d8555dcda8ae59a5ff37a
Artur Smęt <artur.smet@mirumee.com>
2014-04-10 17:40:02 +0200

    Return chunked response, to keep connection alive

--- aldryn_wordpress_import/admin.py
+++ aldryn_wordpress_import/admin.py
@@ -1,19 +1,34 @@
 from django.contrib import admin
 from django.core.urlresolvers import reverse
+from django.contrib import messages
+from django.http import HttpResponse
 from django.shortcuts import redirect
 from .models import WordPressImport
 from .utils import WordpressParser
 
 
 class WordPressImportAdmin(admin.ModelAdmin):
-    list_display = ('author', 'created', 'imported')
+    list_display = ('author', 'created', 'xml_file', 'imported')
     raw_id_fields = ['author']
+
+    def content_iterator(self, request, *args, **kwargs):
+        yield ''
+        yield super(WordPressImportAdmin, self).add_view(
+            request, *args, **kwargs).content
 
     def add_view(self, request, *args, **kwargs):
         data = request.GET.copy()
         data['author'] = request.user.id  # default author is logged-in user
         request.GET = data
-        return super(WordPressImportAdmin, self).add_view(request, *args, **kwargs)
+        if request.method == 'POST':
+            content_iterator = self.content_iterator(request, *args, **kwargs)
+            response = HttpResponse(content_iterator, status=302)
+            response['Location'] = reverse(
+                'admin:aldryn_wordpress_import_wordpressimport_changelist')
+            messages.success(request, 'Wordpress Import added succesfully')
+            return response
+        return super(WordPressImportAdmin, self).add_view(
+            request, *args, **kwargs)
 
     def change_view(self, request, object_id, form_url='', extra_context=None):
         if request.method == 'POST' and 'execute' in request.POST:
@@ -29,7 +44,5 @@ class WordPressImportAdmin(admin.ModelAdmin):
             return redirect(url)
         return super(WordPressImportAdmin, self).change_view(
             request, object_id, form_url, extra_context)
-
-
 
 admin.site.register(WordPressImport, WordPressImportAdmin)
